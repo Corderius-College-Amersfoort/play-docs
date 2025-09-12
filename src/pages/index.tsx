0d4f491dec2b86cc3487d39a077faac9f7a0c7e2
Marten Postma <postmam@corderius.nl>
2025-09-12 13:44:21 +0200

    Update link to documentation in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
                 <div className={styles.buttons}>
                     <Link
                         className="button button--secondary button--lg"
-                        to="/play-docs/docs/category/installatie-corderius-play">
+                        to="/docs/category/installatie-corderius-play">
                         Documentatie
                     </Link>
                 </div>
